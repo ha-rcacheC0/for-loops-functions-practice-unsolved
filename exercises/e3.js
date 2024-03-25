e32e30bb57ab9c0306136b6bd2e03796e7d75508
Lucas Keller <148141760+ha-rcacheC0@users.noreply.github.com>
2024-03-25 15:39:11 -0500

    saving completed exercises

--- exercises/e3.js
+++ exercises/e3.js
@@ -10,8 +10,12 @@
 
 export function getAverage(array) {
   // Your code goes here...
-
-}
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  };
+  return sum/array.length;
+};
 
 
 /** 
@@ -23,8 +27,15 @@ export function getAverage(array) {
 
 export function getStringSum(str) {
   // Your code goes here...
-
-}
+    let sum = 0;
+    for (let i = 0; i < str.length; i++) {
+      let parseChar = parseInt(str[i]);
+      if (!isNaN(parseChar)) {
+        sum += parseChar;
+      };
+    };
+    return sum;
+};
 
 
 // === TEST YOURSELF ===
